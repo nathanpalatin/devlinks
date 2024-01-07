f5dff2cb0c37d313f29cad0cc2d9a64cfd60c9fe
Nathan Palatin <nath.palatin@gmail.com>
2024-01-07 14:10:20 -0300

    [fix] toggle icons

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
-function toggle() {
+const toggle = () => {
   const content = document.querySelector('body')
 
   content.classList.toggle("light")
 
   const img = document.querySelector('.profileimg')
+  const icon = document.querySelector('ion-icon')
+
 
   if (content.classList.contains("light")) {
     img.setAttribute("src", "https://github.com/nathanpalatin.png")
+    icon.setAttribute("name", "moon-outline")
+
   } else {
     img.setAttribute("src", "https://media.licdn.com/dms/image/D4D03AQGoT9B1dlbm4w/profile-displayphoto-shrink_200_200/0/1699448287507?e=1710374400&v=beta&t=1fkPW5B9VJ-AUvSlzCjNdgnqu81al8qJaS2C2dkSEqI")
+    icon.setAttribute("name", "sunny-outline")
   }
 }
